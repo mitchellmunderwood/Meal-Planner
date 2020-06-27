5b8ad88a401e2b179df97877cf8cdfdfb48b8db6
Dwayne Fonville <d.fonville18@gmail.com>
2020-06-27 10:30:38 -0400

    update JS and add navbar

--- home.js
+++ home.js
@@ -1,12 +1,12 @@
-$("#button").on("click", function () {
+$("#button").on("click", function (liquorRecipes) {
   $(this);
 });
-$("#button").on("click", function () {
+$("#button").on("click", function (mealRecipes) {
   $(this);
 });
-$("#button").on("click", function () {
+$("#button").on("click", function (resetaurants) {
   $(this);
 });
-$("#button").on("click", function () {
+$("#button").on("click", function (BAC) {
   $(this);
 });
